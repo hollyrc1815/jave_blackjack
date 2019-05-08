ff0893a48634bc4473fb7517de2c46d2e5230986
hollyrc1815 <hollyrc1815@gmail.com>
2019-05-07 21:33:03 -0500

    blackjack v 2.0 cards are now added to arrays and loops total the sums of the arrays

--- src/blackjack.java
+++ src/blackjack.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class blackjack {
 	
-   
+	static ArrayList<Integer>userHand = new ArrayList<Integer>();
+	static ArrayList<Integer>dealerHand = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 	
@@ -10,11 +11,15 @@ public class blackjack {
 		int userCard2 = dealCards();
 		int dealerCard1 = dealCards();
         int dealerCard2 = dealCards();
+        int userArraySum;
+        int dealerArraySum = dealerHand(dealerCard1, dealerCard2);
+        
         
 		dealerCards(dealerCard1, dealerCard2);
 		playersCards(userCard1, userCard2);
-		//int userCard3 = getOneCard();
+
 		int thirdCard = addCard(drawCard());
+		userArraySum = playerHand(userCard1, userCard2, thirdCard);
 		int newtotal = userCard1 + userCard2;
 		int sum = newtotal + thirdCard;
 		int dealerSum = dealerCard1 + dealerCard2;
@@ -30,33 +35,53 @@ public class blackjack {
 	}//end of int dealCards()
         
     public static void dealerCards(int dealerCard1, int dealerCard2) {
-//        int dealerCard1 = dealCards();
-//        int dealerCard2 = dealCards();
+
         System.out.println("dealers shows " + dealerCard1);
         System.out.println("dealer hides " + dealerCard2);
         //System.out.println("dealers total is " + dealerCard1 +  dealerCard2);
-//        int dealerSum = dealerCard1 + dealerCard2;  
-//        return dealerSum;
+
    
     }//end of dealer hand
+    
+    public static int dealerHand(int dealerCard1, int dealerCard2) {
+        dealerHand.add(dealerCard1);
+        dealerHand.add(dealerCard2);
+        int dealerArraySum = 0;
+        for (int i : dealerHand)
+        {
+        	dealerArraySum += i;
+        }
+        return dealerArraySum;   
        
-	
+    }//end of dealerHand
 	public static void playersCards(int userCard1, int userCard2) {
-//		int userCard1 = dealCards();
-//        int userCard2 = dealCards();
-        //int playerCards[] = {}; 
+
         System.out.println("user got " + userCard1 + " and a " + userCard2);
-//        int sum = userCard1 + userCard2;
-//        return  sum;
-        
+
 	}//end of players cards
+	
+	public static int playerHand(int userCard1, int userCard2, int thirdCard) {		
+        userHand.add(userCard1);
+        userHand.add(userCard2);
+        userHand.add(thirdCard);
+        
+        
+        int userArraySum = 0;
+        for (int i : userHand)
+        {
+        	userArraySum += i;
+        	//System.out.println(userArraySum);
+        }
+        //System.out.println(userArraySum);
+        return userArraySum;   
+	}//end of player hand
 
 	public static String drawCard() {
 	  Scanner myObj = new Scanner(System.in);  // Create a Scanner object
       System.out.println("Would you like to draw a card? yes or no");
 
       String drawCard = myObj.nextLine();  // Read user input
-      //System.out.println("You are getting a card");// Output user input
+
       String userAnswer = drawCard;
       return userAnswer; 
 	 
@@ -64,9 +89,9 @@ public class blackjack {
 	
 	public static int addCard(String userAnswer) {
 		if(userAnswer.equalsIgnoreCase("yes")) { 
-			int userCard3 = getOneCard();
-			System.out.println("Your Third Card is " + userCard3);
-			return userCard3;
+			int newCard3 = getOneCard();
+			System.out.println("Your Third Card is " + newCard3);
+			return newCard3;
 		}
 		else {
 			System.out.println("no card for you!");
@@ -76,18 +101,16 @@ public class blackjack {
 	
 	public static int getOneCard() {
 		Random rand = new Random();
-        int userCard3 = rand.nextInt(10) +1;
-        return userCard3; 
+        int newCard3 = rand.nextInt(10) +1;
+        return newCard3; 
 	}
-//	System.out.println(playersCards(); + userCard3);
+
 	public static void playGame(int dealerSum, int sum) {
 		
-		if(dealerSum < sum && dealerSum <= 21 && sum < 21) {
+		if(dealerSum < sum && dealerSum <= 21 && sum <= 21) {
 			System.out.println("YOU WIN!!");
 		}
-//		if(dealerSum < 17 && sum < 21) {
-//			getOneCard();
-//		}		
+	
 		if(dealerSum > sum && dealerSum <= 21 || sum >= 22) {
 			System.out.println("YOU LOSE!!");
 		}
